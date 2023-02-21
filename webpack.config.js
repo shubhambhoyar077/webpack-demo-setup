b026b4b3a238b78e3edba4f40042d43485bc4a71
shubham <shubham.bhoyar077@gmail.com>
2023-02-21 13:55:15 +0530

    Add setup for loder for css file

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,22 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+
+    static: './dist',
+
+  },
+  // optimization: {
+
+  //   runtimeChunk: 'single',
+
+  // },
 };
