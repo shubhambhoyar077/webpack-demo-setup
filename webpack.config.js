88db16efb8210ed73984f777dc0ea6f90b16c0ca
shubham <shubham.bhoyar077@gmail.com>
2023-02-21 13:43:05 +0530

    Add html webpack plugin and ipdated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      // title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
